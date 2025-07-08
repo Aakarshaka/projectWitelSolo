aa8fa1ca99d58ec0edef9c9d734996f26c215653
valdaveisa <valdaveisa751@gmail.com>
2025-07-08 14:50:54 +0700

    tot

---
+++ app/Http/Controllers/SupportneededController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\supportneeded;
+use Illuminate\Http\Request;
+
+class SupportneededController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(supportneeded $supportneeded)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(supportneeded $supportneeded)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, supportneeded $supportneeded)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(supportneeded $supportneeded)
+    {
+        //
+    }
+}
