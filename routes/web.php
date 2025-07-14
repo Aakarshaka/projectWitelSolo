9c492d283374459fc40722684dc0620df6c909e7
Arya Sabda <147800904+Aakarshaka@users.noreply.github.com>
2025-07-14 09:16:19 +0700

    kocak

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,12 @@ Route::view('/', 'auth.login');
 Route::view('/auth/login', 'auth.login')->name('login');
 Route::view('/auth/register', 'auth.register')->name('register');
 
+Route::get('/supportneeded/detail', [SupportneededController::class, 'getDetail'])->name('supportneeded.detail');
+
 //Route::resource('warroom', WarroomController::class);
 Route::resource('witel', WitelController::class);
 Route::resource('supportneeded', SupportneededController::class);
+
 Route::resource('newsummary', SumController::class);
 
 Route::get('dashboard/newdashboard', function () {
