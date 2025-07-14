953877c148b1e810565230960d1aec1330ef341f
Arya Sabda <147800904+Aakarshaka@users.noreply.github.com>
2025-07-14 08:34:33 +0700

    cek

--- resources/views/layouts/newsidebar.blade.php
+++ resources/views/layouts/newsidebar.blade.php
@@ -33,8 +33,8 @@
             </a>
         </li>
 
-        <li class="{{ Request::is('summary.newsummary') ? 'active' : '' }}">
-            <a href="{{ url('summary/newsummary') }}">
+        <li class="{{ Request::is('newsummary') ? 'active' : '' }}">
+            <a href="{{ url('/newsummary') }}">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M12 16v5" />
                     <path d="M16 14v7" />
--- routes/web.php
+++ routes/web.php
@@ -23,37 +23,10 @@ Route::view('/', 'auth.login');
 Route::view('/auth/login', 'auth.login')->name('login');
 Route::view('/auth/register', 'auth.register')->name('register');
 
-// ==================
-// Dashboard
-// ==================
-Route::view('/dashboard', 'dashboard')->name('dashboard');
-Route::view('/warroom', 'warroom')->name('warroom');
-
-// ==================
-// Support Needed
-// ==================
-Route::get('supportNeeded/summary', [SummaryController::class, 'index'])->name('summary.index');
-
-// ==================
-// Resource Controllers (CRUD)
-// ==================
-Route::get('/supportneeded/detail', [SupportneededController::class, 'getDetail'])->name('supportneeded.detail');
-
-Route::resource('snunit', SnunitController::class);
-Route::resource('snam', SnamController::class);
-Route::resource('sntelda', SnteldaController::class);
-Route::resource('tifta', TiftaController::class)->parameters(['tifta' => 'tifta']);
-Route::resource('treg', TregController::class);
-Route::resource('tsel', TselController::class);
-Route::resource('gsd', GsdController::class);
 //Route::resource('warroom', WarroomController::class);
 Route::resource('witel', WitelController::class);
 Route::resource('supportneeded', SupportneededController::class);
-
-Route::get('summary/newsummary', function () {
-    return view('summary.newsummary');
-});
-Route::get('summary/newsummary', [SumController::class, 'index']);
+Route::resource('newsummary', SumController::class);
 
 Route::get('dashboard/newdashboard', function () {
     return view('dashboard.newdashboard');
